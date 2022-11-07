802405c276c678593bfde928ab5e3465505fff6a
ruthwekesa <wekesaruth569@gmail.com>
2022-11-07 10:11:53 +0300

    models done

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,16 @@
 class Product < ActiveRecord::Base
-    
+    has_many :reviews
+    has_many :users, through: :reviews
+
+
+def leave_review(user, star_rating, comment)
+ Review.create(star_rating: star_rating, comment: comment, user_id: user.id, product_id: self.id)
+end
+def print_all_reviews
+     self.reviews.collect{|review| "Review for #{review.product.name} by #{review.user.name}: #{review.star_rating}. #{review.comment}"}
+end
+
+def average_rating
+self.reviews.average(:star_rating).to_f
+end
 end
